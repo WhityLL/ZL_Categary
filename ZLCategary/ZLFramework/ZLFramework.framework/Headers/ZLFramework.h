6342205c1d38ed7ef8f46cfb2c1772f3831cd06a
whity <pake@uxin.com>
2021-03-28 18:18:28 +0800

    [comments]：framework [jira_id]：none [version]：

---
+++ ZLCategary/ZLFramework/ZLFramework.framework/Headers/ZLFramework.h
@@ -0,0 +1,22 @@
+//
+//  ZLFramework.h
+//  ZLFramework
+//
+//  Created by Liu Lei on 2021/3/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZLFramework.
+FOUNDATION_EXPORT double ZLFrameworkVersionNumber;
+
+//! Project version string for ZLFramework.
+FOUNDATION_EXPORT const unsigned char ZLFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZLFramework/PublicHeader.h>
+
+/** 所有需要暴露出去的头文件必须在这里导入 */
+#import <ZLFramework/ZLClassA.h>"
+#import <ZLFramework/ZLClassB.h>"
+
+
